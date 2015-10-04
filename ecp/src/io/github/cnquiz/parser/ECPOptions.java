0d9d8f1b6dbcd665695e10f6a899613ff17d5c9f
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-10-04 08:57:30 +0100

    ECP default port number updated.

--- ecp/src/io/github/cnquiz/parser/ECPOptions.java
+++ ecp/src/io/github/cnquiz/parser/ECPOptions.java
@@ -3,6 +3,7 @@ package io.github.cnquiz.parser;
 public final class ECPOptions {
 
     private static final int GROUP_NUMBER = 42;
+    private static final int PORT_BASE = 8000;
     private final int portNumber;
 
     public ECPOptions(int portNumber) {
@@ -14,7 +15,7 @@ public final class ECPOptions {
     }
 
     public ECPOptions() {
-        this(GROUP_NUMBER);
+        this(PORT_BASE + GROUP_NUMBER);
     }
 
     public int getPortNumber() {
