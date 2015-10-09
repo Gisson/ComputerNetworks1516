c9064443af38ee21bfe874e31de68e92725b6b45
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-10-09 17:05:09 +0100

    :door: Default port is now 5033. Closes #35

--- ecp/src/io/github/cnquiz/parser/ECPOptions.java
+++ ecp/src/io/github/cnquiz/parser/ECPOptions.java
@@ -3,7 +3,7 @@ package io.github.cnquiz.parser;
 public final class ECPOptions {
 
     private static final int GROUP_NUMBER = 33;
-    private static final int PORT_BASE = 8000;
+    private static final int PORT_BASE = 5000;
     private final int portNumber;
 
     public ECPOptions(int portNumber) {
